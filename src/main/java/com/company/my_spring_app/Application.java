f6b8d086f14871c27e19b4f983485b76f4bd8d0e
lukesundarcorp <lukesundarcorp@gmail.com>
2023-11-27 18:13:03 +0530

    My First Commit

---
+++ src/main/java/com/company/my_spring_app/Application.java
@@ -0,0 +1,15 @@
+package com.company.my_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
